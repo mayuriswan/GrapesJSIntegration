b23bc6ca7448cd257e0e82ee7e8a3dcb562f03ae
Ayoub El haoussi <aelhaoussi@sqli.com>
2024-09-05 04:28:23 +0100

    cutomize the layout

--- GrapesJSIntegration/wwwroot/js/site.js
+++ GrapesJSIntegration/wwwroot/js/site.js
@@ -3,6 +3,16 @@
         container: '#gjs',
         components: '<div class="txt-red">Hello world!</div>',
         style: '.txt-red{color: red}',
+        plugins: ['grapesjs-preset-webpage'],
+        pluginsOpts: {
+            'grapesjs-preset-webpage': {
+                blocks: ['link-block', 'quote', 'text-basic'], // Add these blocks
+                modalImportTitle: 'Custom Import Title',       // Set custom title for the import modal
+                modalImportButton: 'Custom Import',            // Set custom button text for the import modal
+                textCleanCanvas: 'Do you really want to clear the canvas?', // Custom text for cleaning the canvas
+                useCustomTheme: true,   
+            }
+        }
         // Add other configurations
     });
 }
