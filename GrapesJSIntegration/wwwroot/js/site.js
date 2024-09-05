c05c4b497c017d5e7ac60813346d9ec6067cec83
Ayoub El haoussi <aelhaoussi@sqli.com>
2024-09-05 03:59:34 +0100

    first

---
+++ GrapesJSIntegration/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿window.initializeGrapesJS = function () {
+    const editor = grapesjs.init({
+        container: '#gjs',
+        components: '<div class="txt-red">Hello world!</div>',
+        style: '.txt-red{color: red}',
+        // Add other configurations
+    });
+}
